42b3aed29f7d5c167b8de0713f62aa6f373a1be5
jihyunniiii <jita1125@naver.com>
2023-04-03 21:29:32 +0900

    feat : 자기소개 뷰 디자인

---
+++ app/src/main/java/org/android/go/sopt/SelfIntroductionActivity.kt
@@ -0,0 +1,11 @@
+package org.android.go.sopt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelfIntroductionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_self_introduction)
+    }
+}
