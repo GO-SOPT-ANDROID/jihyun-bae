8ddf0d2b07a85202fcf898fcaa35330778590a80
jihyunniiii <jita1125@naver.com>
2023-04-06 15:15:05 +0900

    feat : 자동 로그인 구현

--- app/src/main/java/org/android/go/sopt/LoginActivity.kt
+++ app/src/main/java/org/android/go/sopt/LoginActivity.kt
@@ -1,7 +1,9 @@
 package org.android.go.sopt
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
@@ -29,6 +31,7 @@ class LoginActivity : AppCompatActivity() {
         setActivityResult()
         clickSignUp()
         clickLogin()
+        setAutoLogin()
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
@@ -70,6 +73,8 @@ class LoginActivity : AppCompatActivity() {
             if (id == savedId && pw == savedPw) {
                 Toast.makeText(this, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show()
 
+                saveAutoLoginInfo()
+
                 val intent = Intent(this, SelfIntroductionActivity::class.java).apply {
                     putExtra("name", savedName)
                     putExtra("specialty", savedSpecialty)
@@ -84,5 +89,33 @@ class LoginActivity : AppCompatActivity() {
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         currentFocus?.clearFocus()
+    }
+
+    private fun saveAutoLoginInfo() {
+        val sharedPreference = getSharedPreferences("autoLogin", Activity.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreference.edit()
+        editor.putString("id", savedId)
+        editor.putString("pw", savedPw)
+        editor.putString("name", savedName)
+        editor.putString("specialty", savedSpecialty)
+        editor.commit()
+    }
+
+    private fun setAutoLogin() {
+        val autoLoginInfo = getSharedPreferences("autoLogin", MODE_PRIVATE)
+        val autoLoginId = autoLoginInfo.getString("id", "").toString()
+        val autoLoginPw = autoLoginInfo.getString("pw", "").toString()
+        val autoLoginName = autoLoginInfo.getString("name", "").toString()
+        val autoLoginSpecialty = autoLoginInfo.getString("specialty", "").toString()
+
+        if (autoLoginId != "" && autoLoginPw != "") {
+            val intent = Intent(this, SelfIntroductionActivity::class.java).apply {
+                putExtra("name", autoLoginName)
+                putExtra("specialty", autoLoginSpecialty)
+            }
+            startActivity(intent)
+            Toast.makeText(this, "자동 로그인되었습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
