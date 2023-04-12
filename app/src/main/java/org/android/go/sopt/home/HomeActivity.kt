1d66babb7ed9ef47b61ff7d43d60d908fff5f6e2
jihyunniiii <jita1125@naver.com>
2023-04-12 22:09:54 +0900

    feat : BottomNavigation 구현

--- app/src/main/java/org/android/go/sopt/home/HomeActivity.kt
+++ app/src/main/java/org/android/go/sopt/home/HomeActivity.kt
@@ -2,19 +2,54 @@ package org.android.go.sopt.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import org.android.go.sopt.R
+import org.android.go.sopt.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        val homeFragment = HomeFragment()
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main)
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_home_main)
         if (currentFragment == null) {
             supportFragmentManager.beginTransaction()
-                .add(R.id.fcv_main, homeFragment)
+                .add(R.id.fcv_home_main, HomeFragment())
                 .commit()
         }
+
+        clickNav()
+    }
+
+    private fun clickNav() {
+        binding.bnvHomeMain.setOnItemSelectedListener { item ->
+            changeFragment(
+                when (item.itemId) {
+                    R.id.menu_home -> {
+                        HomeFragment()
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.menu_search -> {
+                        SearchFragment()
+                        return@setOnItemSelectedListener true
+                    }
+                    else -> {
+                        GalleryFragment()
+                        return@setOnItemSelectedListener true
+                    }
+                }
+            )
+            false
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fcv_home_main, fragment)
+            .commit()
     }
 }
