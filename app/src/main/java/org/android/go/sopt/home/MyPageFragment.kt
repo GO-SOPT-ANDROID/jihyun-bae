40476f31e5648d7c095d2a970831790774d8751a
jihyunniiii <jita1125@naver.com>
2023-05-08 08:56:37 +0900

    feat : Dialog 구현 및 자동 로그인 상태 해제 구현

--- app/src/main/java/org/android/go/sopt/home/MyPageFragment.kt
+++ app/src/main/java/org/android/go/sopt/home/MyPageFragment.kt
@@ -6,12 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import org.android.go.sopt.databinding.FragmentMyPageBinding
+import org.android.go.sopt.home.dialog.LogoutDialog
 
 
 class MyPageFragment : Fragment() {
     private var _binding: FragmentMyPageBinding? = null
     private val binding: FragmentMyPageBinding
         get() = requireNotNull(_binding) { "앗 ! _binding이 null이다 !" }
+    private lateinit var dialog: LogoutDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,10 +26,22 @@ class MyPageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setMyPageLogoutBtnClickListener()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setMyPageLogoutBtnClickListener() {
+        binding.btnMyPageLogout.setOnClickListener {
+            showLogOutDialog()
+        }
+    }
+
+    private fun showLogOutDialog() {
+        dialog = LogoutDialog(requireContext())
+        dialog.show()
     }
 }
