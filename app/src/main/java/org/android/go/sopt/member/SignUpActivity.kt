6fa98ad76bc9bbfd5ab4febbbc9cc571f57d6214
jihyunniiii <jita1125@naver.com>
2023-05-11 10:23:57 +0900

    feat : 회원가입 API 통신 구현

--- app/src/main/java/org/android/go/sopt/member/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/member/SignUpActivity.kt
@@ -1,18 +1,24 @@
 package org.android.go.sopt.member
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.MotionEvent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import org.android.go.sopt.R
+import org.android.go.sopt.data.remote.api.ServicePool
+import org.android.go.sopt.data.remote.model.RequestSignUpDto
+import org.android.go.sopt.data.remote.model.ResponseSignUpDto
 import org.android.go.sopt.databinding.ActivitySignUpBinding
 import org.android.go.sopt.util.extension.hideKeyboard
 import org.android.go.sopt.util.extension.showSnackBar
+import org.android.go.sopt.util.extension.showToast
+import retrofit2.Call
+import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
     private val viewModel: MemberViewModel by viewModels()
+    private val signUpService = ServicePool.signUpService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,17 +39,42 @@ class SignUpActivity : AppCompatActivity() {
     private fun clickSignUp() {
         binding.btnSignUpComplete.setOnClickListener {
             if (viewModel.isSignUpEnabled()) {
-                val intent = Intent(this, LoginActivity::class.java).apply {
-                    putExtra("id", binding.etSignUpId.text.toString())
-                    putExtra("pw", binding.etSignUpPw.text.toString())
-                    putExtra("name", binding.etSignUpName.text.toString())
-                    putExtra("specialty", binding.etSignUpSpecialty.text.toString())
-                }
-                setResult(RESULT_OK, intent)
-                finish()
+                completeSignUp()
             } else {
                 binding.root.showSnackBar(getString(R.string.sign_up_fail))
             }
         }
+    }
+
+    private fun completeSignUp() {
+        signUpService.login(
+            with(binding) {
+                RequestSignUpDto(
+                    etSignUpId.text.toString(),
+                    etSignUpPw.text.toString(),
+                    etSignUpName.text.toString(),
+                    etSignUpSpecialty.text.toString()
+                )
+            }
+        ).enqueue(object : retrofit2.Callback<ResponseSignUpDto> {
+            override fun onResponse(
+                call: Call<ResponseSignUpDto>,
+                response: Response<ResponseSignUpDto>
+            ) {
+                if (response.isSuccessful) {
+                    response.body()?.message?.let { showToast(it) } ?: "회원가입에 성공했습니다."
+
+                    if (!isFinishing) finish()
+                } else {
+                    // 실패한 응답
+                    response.body()?.message?.let { showToast(it) } ?: "서버통신 실패(40X)"
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseSignUpDto>, t: Throwable) {
+                t.message?.let { showToast(it) } ?: "서버통신 실패(응답값 X)"
+            }
+
+        })
     }
 }
