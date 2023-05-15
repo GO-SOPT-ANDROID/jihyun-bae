531d890b68680ac407c1a423c7e00dc3e25cb908
jihyunniiii <jita1125@naver.com>
2023-05-15 17:11:31 +0900

    fix : ListUsersService에 어노테이션 이용

--- app/src/main/java/org/android/go/sopt/data/remote/service/ListUsersService.kt
+++ app/src/main/java/org/android/go/sopt/data/remote/service/ListUsersService.kt
@@ -3,8 +3,11 @@ package org.android.go.sopt.data.remote.service
 import org.android.go.sopt.data.remote.model.ResponseListUsersDto
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ListUsersService {
-    @GET("/api/users?page=2")
-    fun getListUsers(): Call<ResponseListUsersDto>
+    @GET("/api/users")
+    fun getListUsers(
+        @Query("page") num: Int = 2
+    ): Call<ResponseListUsersDto>
 }
