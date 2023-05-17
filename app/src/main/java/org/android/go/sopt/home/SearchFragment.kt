8da23a12f3ad2132012ac1c42f521171c5bbeb41
jihyunniiii <jita1125@naver.com>
2023-05-17 23:27:38 +0900

    feat : 카카오 검색 API 통신 구현

--- app/src/main/java/org/android/go/sopt/home/SearchFragment.kt
+++ app/src/main/java/org/android/go/sopt/home/SearchFragment.kt
@@ -5,28 +5,64 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.android.go.sopt.R
+import org.android.go.sopt.data.remote.api.KakaoSearchServicePool
+import org.android.go.sopt.data.remote.model.ResponseKakaoSearchDto
 import org.android.go.sopt.databinding.FragmentSearchBinding
+import org.android.go.sopt.home.adapter.SearchAdapter
+import org.android.go.sopt.util.extension.showToast
+import retrofit2.Call
+import retrofit2.Response
 
 class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding: FragmentSearchBinding
         get() = requireNotNull(_binding) { "앗 ! _binding이 null이다 !" }
+    private val kakaoSearchService = KakaoSearchServicePool.KakaoSearchService
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        searchCheckBtnClickListener()
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    private fun connectAdapter(searchInfo: List<ResponseKakaoSearchDto.Document>) {
+        val searchAdapter = SearchAdapter(requireContext())
+        searchAdapter.submitList(searchInfo)
+        binding.rvSearch.adapter = searchAdapter
+    }
+
+    private fun searchCheckBtnClickListener() {
+        binding.btnSearchCheck.setOnClickListener {
+            kakaoSearchService.getKakaoSearch(searchWord = binding.svSearch.query.toString())
+                .enqueue(object : retrofit2.Callback<ResponseKakaoSearchDto> {
+                    override fun onResponse(
+                        call: Call<ResponseKakaoSearchDto>,
+                        response: Response<ResponseKakaoSearchDto>
+                    ) {
+                        if (response.isSuccessful) {
+                            response.body()?.documents?.let { connectAdapter(it) }
+                        }
+                    }
+
+                    override fun onFailure(call: Call<ResponseKakaoSearchDto>, t: Throwable) {
+                        t.message?.let { requireContext().showToast(it) } ?: run {
+                            requireContext().showToast(getString(R.string.server_communication_on_failure))
+                        }
+                    }
+
+                })
+        }
     }
 }
