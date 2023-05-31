2864bca3b99b95a2b82f87182506af6f1790e8da
jihyunniiii <jita1125@naver.com>
2023-05-31 15:52:04 +0900

    feat : Interceptor

--- app/src/main/java/org/android/go/sopt/data/remote/api/MemberApiFactory.kt
+++ app/src/main/java/org/android/go/sopt/data/remote/api/MemberApiFactory.kt
@@ -3,13 +3,27 @@ package org.android.go.sopt.data.remote.api
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.android.go.sopt.BuildConfig
+import org.android.go.sopt.data.remote.AuthInterceptor
 import org.android.go.sopt.data.remote.service.SignInService
 import org.android.go.sopt.data.remote.service.SignUpService
 import retrofit2.Retrofit
 
 object MemberApiFactory {
     private const val BASE_URL = BuildConfig.AUTH_BASE_URL
+
+    private val client by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor())
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level =
+                        if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+                }
+            ).build()
+    }
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
