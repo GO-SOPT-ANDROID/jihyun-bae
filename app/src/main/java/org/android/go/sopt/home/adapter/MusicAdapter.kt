84487eb5542766cd6b595c753723fc6753cd3f98
jihyunniiii <jita1125@naver.com>
2023-06-23 07:23:35 +0900

    feat : MusicAdapter 생성

---
+++ app/src/main/java/org/android/go/sopt/home/adapter/MusicAdapter.kt
@@ -0,0 +1,47 @@
+package org.android.go.sopt.home.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.api.load
+import org.android.go.sopt.data.remote.model.ResponseMusicDto
+import org.android.go.sopt.databinding.ItemMusicBinding
+import org.android.go.sopt.util.extension.ItemDiffCallback
+
+class MusicAdapter(context: Context) :
+    ListAdapter<ResponseMusicDto.MusicData, RecyclerView.ViewHolder>(
+        ItemDiffCallback<ResponseMusicDto.MusicData>(
+            onContentsTheSame = { old, new -> old == new },
+            onItemsTheSame = { old, new -> old == new }
+        )
+    ) {
+    private val inflater by lazy { LayoutInflater.from(context) }
+
+    class MusicViewHolder(
+        private val binding: ItemMusicBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: ResponseMusicDto.MusicData) {
+            with(binding) {
+                ivItemMusicImg.load(data.url)
+                tvItemMusicTitle.text = data.title
+                tvItemMusicSinger.text = data.singer
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = ItemMusicBinding.inflate(inflater, parent, false)
+        return MusicViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is MusicViewHolder -> holder.onBind(
+                currentList[position]
+            )
+        }
+    }
+
+}
